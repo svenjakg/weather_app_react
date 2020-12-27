d38d0befad3c255bb2d485e74c7b409d6bff04d9
svenjakg <74977917+svenjakg@users.noreply.github.com>
2020-12-27 09:12:20 +0100

    Added basic css styling.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import Weather from "./Weather"
+import Weather from "./Weather";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
         <Weather />
-        <footer>This project was created by Svenja Greif and is <a href="https://github.com/svenjakg/weather_app_react" target="_blank" rel="noreferrer">open source on GitHub</a>.</footer>
+        <footer>
+          This project was created by Svenja Greif and is{" "}
+          <a
+            href="https://github.com/svenjakg/weather_app_react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open source on GitHub
+          </a>
+          .
+        </footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -12,13 +12,22 @@ export default function Weather() {
               className="form-control"
               type="search"
               placeholder="Enter a city ..."
+              autoFocus="on"
             />
           </div>
           <div className="col-2">
-            <input className="btn btn-primary" type="submit" value="Search" />
+            <input
+              className="btn btn-primary w-100"
+              type="submit"
+              value="Search"
+            />
           </div>
           <div className="col-2">
-            <input className="btn btn-success" type="submit" value="Current" />
+            <input
+              className="btn btn-success w-100"
+              type="submit"
+              value="Current"
+            />
           </div>
         </div>
       </form>
@@ -27,13 +36,19 @@ export default function Weather() {
         <li>Sunday, 07:00</li>
         <li>Mostly cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="mostly cloudy"
-          />
-          <span>2 °C | °F</span>
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="mostly cloudy"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">2</span>
+              <span className="unit"> °C | °F</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
