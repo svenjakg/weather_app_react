d10d36b47f416cd51abd8271b6514c6c5a31f500
svenjakg <74977917+svenjakg@users.noreply.github.com>
2020-12-27 07:00:36 +0100

    Included footer with link to GitHub repository.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>This project was created by Svenja Greif and is <a href="https://github.com/svenjakg/weather_app_react" target="_blank" rel="noreferrer">open source on GitHub</a>.</footer>
     </div>
   );
 }
