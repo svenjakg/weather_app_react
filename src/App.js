1d2a49cec133f379cb67fb4f264cc7d85dfde948
svenjakg <74977917+svenjakg@users.noreply.github.com>
2020-12-27 07:13:20 +0100

    Included bootstrap.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>This project was created by Svenja Greif and is <a href="https://github.com/svenjakg/weather_app_react" target="_blank" rel="noreferrer">open source on GitHub</a>.</footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>This project was created by Svenja Greif and is <a href="https://github.com/svenjakg/weather_app_react" target="_blank" rel="noreferrer">open source on GitHub</a>.</footer>
+      </div>
     </div>
   );
 }
