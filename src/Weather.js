3df50c9b8c0e19731a3a83d851b06946cb57f7d7
svenjakg <74977917+svenjakg@users.noreply.github.com>
2020-12-29 13:49:13 +0100

    Bug fix for 'Current' button.

--- src/Weather.js
+++ src/Weather.js
@@ -59,7 +59,7 @@ export default function Weather(props) {
     navigator.geolocation.getCurrentPosition(findLocation);
   }
 
-  if (weatherData.loaded) {
+  if (weatherData.loaded && city === weatherData.city) {
     return (
       <div className="Weather">
         <form onSubmit={handleSubmit}>
